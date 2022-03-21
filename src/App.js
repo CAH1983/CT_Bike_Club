bcaa3aa657651b0b8d735054a7825cd5ba752485
Ho <caroline.ho@bauermedia.co.uk>
2022-03-21 20:07:44 +0000

    added dark mode

--- src/App.js
+++ src/App.js
@@ -8,10 +8,24 @@ function App() {
   const generateNum = () => Math.floor(Math.random() * 11);
   const randomNum = generateNum();
 
+  const [darkMode, setDarkMode] = useState(false);
+
+
   return (
     <div className="App">
       <Header />
-      <main>
+      <main className={darkMode ? "dark-mode" : "light-mode"}>
+
+        <div className="btn-container">
+          <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+          <div className="switch-checkbox">
+            <label className="switch">
+              <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+              <span className="slider round"> </span>
+            </label>
+          </div>
+          <span style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
+        </div>
 
         <ul className='contacts-wrapper'>
 
@@ -21,10 +35,8 @@ function App() {
 
             </li>
           ))}
+
         </ul>
-
-
-
       </main>
     </div>
   );
